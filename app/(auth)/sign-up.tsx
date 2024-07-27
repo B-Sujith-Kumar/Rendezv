4debe18afb2c80d73c8f4dbec8de167d2b54629f
Banda Sujith Kumar <sujith.suave@gmail.com>
2024-07-27 17:16:46 +0530

    FEAT: Added Sign In and Sign Up UI.

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,167 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={styles.container}>
+        <Text
+          style={{ fontFamily: "FontRegular", color: "white", fontSize: 17 }}
+        >
+          Hey there,
+        </Text>
+        <Text
+          style={{
+            fontFamily: "FontBold",
+            color: "white",
+            fontSize: 32,
+            marginVertical: 10,
+          }}
+        >
+          Create an account
+        </Text>
+        <View style={{ width: "100%", padding: 10, gap: 15, marginTop: 20 }}>
+          <View style={styles.inputContainer}>
+            <Feather name="user" size={16} color="#a0a0a0" />
+            <TextInput style={styles.input} placeholder="Name" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Fontisto name="email" size={16} color="#a0a0a0" />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Feather name="lock" size={16} color="#a0a0a0" />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={showPassword ? false : true}
+            />
+            {showPassword && (
+              <TouchableOpacity onPress={() => setShowPassword(false)}>
+                <FontAwesome5 name="eye" size={16} color="#a0a0a0" />
+              </TouchableOpacity>
+            )}
+            {!showPassword && (
+              <TouchableOpacity onPress={() => setShowPassword(true)}>
+                <FontAwesome5 name="eye-slash" size={16} color="#a0a0a0" />
+              </TouchableOpacity>
+            )}
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text
+              style={{ color: "black", fontSize: 18, fontFamily: "FontBold" }}
+            >
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.seperatorContainer}>
+          <View style={styles.seperator}></View>
+          <Text style={{ color: "white" }}>Or</Text>
+          <View style={styles.seperator}></View>
+        </View>
+        <TouchableOpacity style={styles.socialLogin}>
+          <Fontisto name="google" size={24} color="white" />
+          <Text
+            style={{ color: "white", fontSize: 18, fontFamily: "FontBold" }}
+          >
+            Continue with Google
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialLogin}>
+          <FontAwesome name="facebook" size={24} color="white" />
+          <Text
+            style={{ color: "white", fontSize: 18, fontFamily: "FontBold" }}
+          >
+            Continue with Facebook
+          </Text>
+        </TouchableOpacity>
+        <View>
+            <Text style={{ color: "#8d8d8d", marginTop: 20 }}>
+                Already have an account?{" "}
+                <Link href="/sign-in" style={{ color: "white", fontFamily: "FontBold" }}>Login</Link>
+            </Text>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    color: "white",
+  },
+  inputContainer: {
+    backgroundColor: "#202020",
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 20,
+    paddingHorizontal: 18,
+    borderRadius: 15,
+    gap: 10,
+    borderColor: "#353535",
+    fontFamily: "FontRegular",
+  },
+  input: {
+    flex: 1,
+    color: "white",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#f8eb6b",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 19,
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  seperatorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    marginVertical: 30,
+  },
+  seperator: {
+    height: 1,
+    width: "40%",
+    backgroundColor: "#8d8d8d",
+  },
+  socialLogin: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    marginVertical: 10,
+    paddingVertical: 22,
+    borderColor: "#313131",
+    borderWidth: 1,
+    width: "100%",
+    borderRadius: 15,
+  },
+});
+
+export default SignUp;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,11 +28,11 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    FontRegular: require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
-    FontBold: require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
-    FontMedium: require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
-    FontSemiBold: require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
-    ...FontAwesome.font,
+    FontRegular: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    FontBold: require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    FontMedium: require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    FontSemiBold: require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -52,11 +57,22 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ClerkProvider
+        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      >
+        <SignedOut>
+            <Stack>
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            </Stack>
+        </SignedOut>
+        <SignedIn>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            </Stack>
+        </SignedIn>
+      </ClerkProvider>
     </ThemeProvider>
   );
 }
